d6583deed5631d11c98afda57d6d95252a76dcfa
ajayadav1511 <ajayadav1511@gmail.com>
2019-10-28 23:04:55 +0530

    A

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from "./components/Layout/Layout";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 function App() {
   return (
     <div className="App">
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React from "react";
 // import classes from './Toolbar.css';
 // import Logo from '../../Logo/Logo';
 // import NavigationItems from '../NavigationItems/NavigationItems';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
 // import classes3 from './Toolbar.css';
-const toolbar = (props) => {
-   
-  const style1={
+import SignUpForm from "../../Authentication/SignUp";
 
-    fontSize: '25px',
-  }
-  return(
-  <Navbar style={style1} bg="dark" variant="dark">
-  <Navbar.Brand style={style1} href="#home">Navbar</Navbar.Brand>
-  <Nav className="ml-auto">
-    <Nav.Link style={style1} href="#home">Home</Nav.Link>
-    <Nav.Link href="#features" className="mr-sm-2">Features</Nav.Link>
-    <Nav.Link href="#pricing">Pricing</Nav.Link>
-    <Nav.Link href="#login" >LogIn</Nav.Link>
-    <Nav.Link href="#Signup" >SignUp</Nav.Link>
-    
-  </Nav>
-  {/* <Form inline>
+const toolbar = props => {
+  const style1 = {
+    fontSize: "25px"
+  };
+  return (
+    <Navbar style={style1} bg="dark" variant="dark">
+      <Navbar.Brand style={style1} href="#home">
+        Navbar
+      </Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link style={style1} href="#home">
+          Home
+        </Nav.Link>
+        <Nav.Link href="#features" className="mr-sm-2">
+          Features
+        </Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="#login">LogIn</Nav.Link>
+        <Nav.Link href="/signup" component={SignUpForm}>
+          SignUp
+        </Nav.Link>
+        {/* <Route exact path="/signup" component={SignUpForm}>
+          SignUP
+        </Route> */}
+      </Nav>
+      {/* <Form inline>
     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
     <Button variant="outline-info">Search</Button>
   </Form> */}
-</Navbar>
-  )
-  };
+    </Navbar>
+  );
+};
 
 export default toolbar;
