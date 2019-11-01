26f80da289d04a8c1b0c8407867fad70ac3c6abe
sanjaysupanch <sanjay.k17@iiits.in>
2019-11-01 18:41:43 +0530

    sanjay

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import './App.css';
+import React, { Component } from 'react';
+// import Layout from './components/Layout/Layout';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/login/Nav';
+import LoginForm from './components/login/LoginForm';
+import SignupForm from './components/login/SignupForm';
+import './App.css';
+import axios from 'axios';
+axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
-function App() {
-  return (
-    <div className="App">
-      <Layout >
-        {/* <Home >         */}
-      </Layout>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: '',
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: ''
+    };
+  }
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
+  render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case 'signup':
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
+
+    return (
+      <div className="App">
+        <Nav
+          logged_in={this.state.logged_in}
+          display_form={this.display_form}
+          handle_logout={this.handle_logout}
+        />
+        {form}
+        <h3>
+          {this.state.logged_in
+            ? `Hello, ${this.state.username}`
+            : 'Please Log In'}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
